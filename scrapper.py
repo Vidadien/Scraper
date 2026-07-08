14f7cc56a6a3a7c74be430dcff8b8ae9cc83a5e2
Vidadien <jdanzolaa@gmail.com>
2026-07-08 09:31:51 -0500

    actualizacion detector fechas

--- scrapper.py
+++ scrapper.py
@@ -3,7 +3,6 @@ from bs4 import BeautifulSoup
 from pathlib import Path
 import pandas as pd
 from datetime import datetime
-from zoneinfo import ZoneInfo
 fondos=["CAPITAL","ACCIONES GLOBAL","DIVER DINAMICO","RENTA FIJA GLOBAL","RENTA FIJA PESOS","ACCIONES COLOMBIA",
         "DIVER MODERADO","ESTABLE","LIQUIDEZ DOLAR","SOSTENIBLE GLOBAL","FINCA RAIZ INTERNACIONAL","ORO","DIVER CONSERVADOR"]
 archivo = Path(__file__).parent / "fondos.csv"
@@ -13,22 +12,26 @@ else:
     columnas = ["Fecha"] + fondos
     df = pd.DataFrame(columns=columnas)
     df.to_csv(archivo, index=False)
-hoy = datetime.now(ZoneInfo("America/Bogota")).date().isoformat()
-if (df["Fecha"] == hoy).any():
+URL = "https://transacciones.davivienda.com/dinamicos/ComercialServlet?item=fondos&canal=inter&consulta=dafuturo"
+with sync_playwright() as p:
+    browser = p.chromium.launch(headless=False)
+    page = browser.new_page()
+    page.goto(URL)
+    page.wait_for_load_state("networkidle")
+    html = page.content()
+    browser.close()
+soup = BeautifulSoup(html, "html.parser")
+tabla = soup.find_all("table")[0]
+filas = tabla.find_all("tr")
+filahoy = filas[5]
+filahoy = [c.get_text(" ", strip=True) for c in filahoy.find_all(["td", "th"])]
+filahoy = filahoy[1]
+hoy = datetime.strptime(filahoy, "%d/%m/%y").date()
+print(hoy)
+if (df["Fecha"] == str(hoy)).any():
     print("Ya existe un registro para hoy.")
 else:
-    fila1={"Fecha": hoy}
-    URL = "https://transacciones.davivienda.com/dinamicos/ComercialServlet?item=fondos&canal=inter&consulta=dafuturo"
-    with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False)
-        page = browser.new_page()
-        page.goto(URL)
-        page.wait_for_load_state("networkidle")
-        html = page.content()
-        browser.close()
-    soup = BeautifulSoup(html, "html.parser")
-    tabla = soup.find_all("table")[0]
-    filas = tabla.find_all("tr")
+    fila1={"Fecha": str(hoy)}
     for fila in filas:
         columnas = [c.get_text(" ", strip=True) for c in fila.find_all(["td", "th"])]
         if len(columnas) >= 5:
